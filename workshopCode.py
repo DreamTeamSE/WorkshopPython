c0629772ce8c8eeaff1a1a85be8bfa46f46d4623
yuamanda <92806914+yuamanda@users.noreply.github.com>
2022-02-17 20:24:06 -0500

    Update workshopCode.py

--- workshopCode.py
+++ workshopCode.py
@@ -12,5 +12,41 @@ You can return the answer in any order.
 Input: using standard input, take in a space-separated list of numbers
 Output: print out the list in the format '[X, Y]' where X, Y are the two numbers
     that add to the target
+
+
+in:
+2 7 11 11 15
+9
+
+out:
+[0, 1]
 """
+
+def SolveTwoSum(nums, target):
+{
+    #[2, 7, 11, 15]
+    for i, num1 in enumerate(nums):
+    {
+        for j in range (i+1, len(nums)):
+        {
+            if(num1 + nums[j] == target)
+            {
+                return "[%d,%d]" % (i, j)
+            }
+        }
+    }
+    return "impossible"
+}
+
 #single line comment
+
+inLine = input().split("Type the space-seperated list of numbers ") # "2 7 11 15" -> ["2", "7", "11", "15"]
+target = int(input("Type the target number\n")) # target = 9
+
+#for(int i = 0; i < inLine.length(); i++)
+for i in range(len(inLine))
+{
+    inLine[i] = int(inLine[i])
+}
+
+print(SolveTwoSum(inLine, target)
